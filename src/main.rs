7ed3601d08f2672c410e2e925037031f1a355f70
Shirakami Emmmer <yjhdandan@163.com>
2020-04-06 18:51:49 +0800

    open file s

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut source_dir = String::from("/Volumes/Macintosh HD/Users/yangjinghua/Downloads/disk_linker");
+    let target_dir = "/Volumes/milk_tea/disk_linker";
+    let mut filename = source_dir.push_str("/test.PNG");
+
+    // println!("{}", filename);
+
+    let file = std::fs::File::open("/Volumes/Macintosh HD/Users/yangjinghua/Downloads/disk_linker/test.PNG").unwrap();
+    println!("文件打开成功：{:?}", file);
 }
