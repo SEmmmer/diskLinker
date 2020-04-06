54bca084db6e78e447b5d6fcdc531ec9f8d40724
Shirakami Emmmer <yjhdandan@163.com>
2020-04-06 22:40:20 +0800

    cope to disk s

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::io::{Write, Read};
 
 fn main() {
-    let filepath = String::from("/Volumes/Macintosh HD/Users/yangjinghua/Downloads/disk_linker/test.PNG");
+    let source_filepath = String::from("/Volumes/Macintosh HD/Users/yangjinghua/Downloads/disk_linker/");
+    let target_filepath = String::from("/Volumes/milk_tea/disk_linker/");
 
-    let mut file = std::fs::File::open(filepath).unwrap();
+    let source_file = String::from("test.PNG");
+    let target_file = String::from("new.PNG");
 
-    let mut new_file = std::fs::File::create("data.PNG").expect("create failed");
+    let mut file = std::fs::File::open(source_filepath + &source_file).unwrap();
 
-    println!("文件打开成功：{:?}", file);
+    let mut new_file = std::fs::File::create(target_filepath + &target_file).expect("create failed");
 
     let mut buffer = [0u8];
 
