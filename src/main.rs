87a65d9934b17624160c258cf513e4b079d3e026
Shirakami Emmmer <yjhdandan@163.com>
2020-04-07 17:32:06 +0800

    try to exam file first

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,42 @@
 extern crate crypto;
 
 use std::io::{Write, Read};
-use std::fs::File;
 use crypto::md5::Md5;
 use crypto::digest::Digest;
+// use std::fs::File;
 
 fn main() {
     let source_file = String::from("test.PNG");
-    let target_file = String::from("new.PNG");
-    copy_file(source_file, target_file);
+    let target_file = String::from("test.PNG");
+    if same_file(get_source(source_file.clone()), get_target(target_file.clone())) {
+        println!("The file already exist.");
+    } else {
+        copy_file(source_file, target_file);
+    }
 }
 
-fn md5_code(mut file: File) {
+fn md5_code(file: &mut std::fs::File) -> String {
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
     let mut hasher = Md5::new();
     hasher.input(&buffer);
-    println!("{}", hasher.result_str());
+    hasher.result_str()
+}
+
+fn same_file(str1: String, str2: String) -> bool {
+    let mut file1 = std::fs::File::open(str1).unwrap();
+    let mut file2 = std::fs::File::open(str2).unwrap();
+    if md5_code(&mut file1) == md5_code(&mut file2) { true } else { false }
 }
 
 fn copy_file(source_file: String, target_file: String) {
-    let source_filepath = String::from("/Volumes/Macintosh HD/Users/yangjinghua/Downloads/disk_linker/");
-    let target_filepath = String::from("/Volumes/data_disk/disk_linker/");
+    println!("===== copy start =====");
 
-    let mut old_file = std::fs::File::open(source_filepath + &source_file).unwrap();
-    md5_code(old_file);
-    let mut new_file = std::fs::File::create(target_filepath + &target_file).expect("create failed");
+    let source = get_source(source_file);
+    let target = get_target(target_file);
+
+    let mut old_file = std::fs::File::open(source).unwrap();
+    let mut new_file = std::fs::File::create(target).expect("create failed");
 
     let mut buffer = [0u8];
 
@@ -34,4 +45,15 @@ fn copy_file(source_file: String, target_file: String) {
         new_file.write(&buffer[..bytes]).unwrap();
         if bytes < buffer.len() { break; }
     }
+    println!("===== copy over =====");
+}
+
+fn get_source(source_file: String) -> String {
+    let source_filepath = String::from("/Volumes/Macintosh HD/Users/yangjinghua/Downloads/disk_linker/");
+    source_filepath + &source_file
+}
+
+fn get_target(target_file: String) -> String {
+    let target_filepath = String::from("/Volumes/data_disk/disk_linker/");
+    target_filepath + &target_file
 }
